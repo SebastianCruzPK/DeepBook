cc58ec1d1d3992c3f344d191d579f92e18702d39
Ch4le <94202425+Ch4le@users.noreply.github.com>
2022-04-25 14:52:33 -0400

    Update voyatenersuerte.js

--- js/voyatenersuerte.js
+++ js/voyatenersuerte.js
@@ -9,3 +9,20 @@ $("#enviar").click(function(){
     });
 
 });
+var contenido = document.querySelector('#contenido')
+function traer() {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results['0'])
+            contenido.innerHTML =
+                `<img src="${data.results['0'].picture.large}" class="img-fluid rounded-circle">
+    <p>Nombre: ${data.results['0'].name.first}</p>
+    <p>Apellido: ${data.results['0'].name.last}</p>
+    <p>Ciudad: ${data.results['0'].location.city}</p>
+    <p>Email: ${data.results['0'].email}</p>
+    <p>Sexo: ${data.results['0'].gender}</p>
+    <p>Edad: ${data.results['0'].dob.age}</p>
+    <p>Id: ${data.results['0'].login.username}</p>`
+        })
+}
